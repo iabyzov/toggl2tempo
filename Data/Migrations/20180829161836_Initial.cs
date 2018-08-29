2e6107a58500db32ad95b6c1e720013bc07eaa87
Ilya Abyzov <iabyzov@oneincsystems.com>
2018-08-29 23:55:25 +0700

    Fixed initial migraiton

--- Data/Migrations/20180829161836_Initial.cs
+++ Data/Migrations/20180829161836_Initial.cs
@@ -9,35 +9,7 @@ namespace Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "User",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
-                    Email = table.Column<string>(nullable: true),
-                    TempoToken = table.Column<string>(nullable: true),
-                    TogglToken = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_User", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Worklog",
-                columns: table => new
-                {
-                    Id = table.Column<long>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
-                    MasterId = table.Column<long>(nullable: false),
-                    SecondaryId = table.Column<long>(nullable: false),
-                    UpdatedOn = table.Column<DateTime>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Worklog", x => x.Id);
-                });
+            
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
